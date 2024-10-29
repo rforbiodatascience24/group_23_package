78bd2c94863a21760602c2dc27d41165a0154fe1
GioeleTiraboschi1905 <s242150@dtu.dk>
2024-10-29 09:54:27 +0100

    Created the codons variable

---
+++ R/data.R
@@ -0,0 +1,6 @@
+#' Codon table
+#'
+#' Here we have all the codons that code for proteins
+#'
+#' @source \url{https://www.ncbi.nlm.nih.gov/Taxonomy/Utils/wprintgc.cgi?chapter=tgencodes#SG1}
+"codons"
---
+++ data-raw/codons.R
@@ -0,0 +1,3 @@
+## code to prepare `codons` dataset goes here
+
+usethis::use_data(codons, overwrite = TRUE)
